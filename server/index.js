9033d206daa4bf270f5a11eba2957e5e761ceda1
ABZZDEV <42191357+Abzzdev@users.noreply.github.com>
2021-08-11 17:15:12 +0100

    Test route and index.js completed

--- server/index.js
+++ server/index.js
@@ -1,4 +1,22 @@
 import express from 'express';
-import bodyParser from 'body-parser';
 import mongoose from 'mongoose';
 import cors from 'cors';
+
+import teamRoutes from './routes/teams.js'
+
+const app = express();
+
+app.use(express.json({ limit: "30mb", extended: true }));
+app.use(express.urlencoded({ limit: "30mb", extended: true }));
+app.use(cors());
+
+app.use('/teams', teamRoutes);
+
+const CONNECTION_URL = 'mongodb+srv://HuntingGroundsDBAdmin:zHYRdt19zXK2x20Z@hunting-grounds-cluster.dpheu.mongodb.net/myFirstDatabase?retryWrites=true&w=majority'
+const PORT = process.env.PORT || 5000;
+
+mongoose.connect(CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() => app.listen(PORT, () => console.log(`Server Running on Port: http://localhost:${PORT}`)))
+  .catch((error) => console.log(`${error} did not connect`));
+
+mongoose.set('useFindAndModify', false);
