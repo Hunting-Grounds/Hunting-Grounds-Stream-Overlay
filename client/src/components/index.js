60d54280e735a54b56c072d9245656dddd584c75
ABZZDEV <42191357+Abzzdev@users.noreply.github.com>
2021-08-25 10:14:18 +0100

    Added blank control panel page

--- client/src/components/index.js
+++ client/src/components/index.js
@@ -4,5 +4,7 @@ export { default as TeamManagement } from "./TeamManagement/TeamManagement";
 
 export { default as PlayerManagement } from "./PlayerManagement/PlayerManagement";
 
+export { default as ControlPanel } from "./ControlPanel/ControlPanel"
+
 export { default as OverlayMenu } from "./Overlays/OverlayMenu/OverlayMenu";
 export { default as LeagueTable } from "./Overlays/LeagueTable/LeagueTable";
