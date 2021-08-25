8abb0d89cce14249a354a81734f4db790136c8dd
ABZZDEV <42191357+Abzzdev@users.noreply.github.com>
2021-08-25 16:05:04 +0100

    Update OverlayMenu.jsx

--- client/src/components/Overlays/OverlayMenu/OverlayMenu.jsx
+++ client/src/components/Overlays/OverlayMenu/OverlayMenu.jsx
@@ -1,9 +1,28 @@
 import React from 'react'
+import 'antd/dist/antd.css';
+import { Divider, Row, Col, Card } from 'antd';
 
 function OverlayMenu() {
     return (
         <div>
-            
+            <Row justify="space-around" gutter={[24, 24]}>
+                <Col>
+                    <Card>
+                        <a><h1>League Table</h1></a>
+                        <Divider />
+                        <a><h1>League Table</h1></a>
+                        <Divider />
+                        <a><h1>League Table</h1></a>
+                        <Divider />
+                        <a><h1>League Table</h1></a>
+                        <Divider />
+                        <a><h1>League Table</h1></a>
+                        <Divider />
+                        <a><h1>League Table</h1></a>
+                        <Divider />
+                    </Card>
+                </Col>
+            </Row>
         </div>
     )
 }
