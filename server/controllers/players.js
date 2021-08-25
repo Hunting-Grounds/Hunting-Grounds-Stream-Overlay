c804aa0cc9a16c8bf2b5f4f8a95a15a92be70b78
ABZZDEV <42191357+Abzzdev@users.noreply.github.com>
2021-08-25 10:17:35 +0100

    Setup player controllers

--- server/controllers/players.js
+++ server/controllers/players.js
@@ -28,9 +28,9 @@ export const getPlayer = async (req, res) => {
 }
 
 export const createPlayer = async (req, res) => {
-    const { title, message, selectedFile, creator, tags } = req.body;
+    const { name, kd, hs, favops, selectedFile } = req.body;
 
-    const newPlayer = new Player({ title, message, selectedFile, creator, tags })
+    const newPlayer = new Player({ name, kd, hs, favops, selectedFile })
 
     try {
         await newPlayer.save();
@@ -43,11 +43,11 @@ export const createPlayer = async (req, res) => {
 
 export const updatePlayer = async (req, res) => {
     const { id } = req.params;
-    const { title, message, creator, selectedFile, tags } = req.body;
+    const { name, kd, hs, favops, selectedFile } = req.body;
     
-    if (!mongoose.Types.ObjectId.isValid(id)) return res.status(404).send(`No team with id: ${id}`);
+    if (!mongoose.Types.ObjectId.isValid(id)) return res.status(404).send(`No player with id: ${id}`);
 
-    const updatedPlayer = { creator, title, message, tags, selectedFile, _id: id };
+    const updatedPlayer = { name, kd, hs, favops, selectedFile, _id: id };
 
     await Player.findByIdAndUpdate(id, updatedPlayer, { new: true });
 
@@ -57,35 +57,11 @@ export const updatePlayer = async (req, res) => {
 export const deletePlayer = async (req, res) => {
     const { id } = req.params;
 
-    if (!mongoose.Types.ObjectId.isValid(id)) return res.status(404).send(`No team with id: ${id}`);
+    if (!mongoose.Types.ObjectId.isValid(id)) return res.status(404).send(`No player with id: ${id}`);
 
     await Player.findByIdAndRemove(id);
 
     res.json({ message: "Player deleted successfully." });
-}
-
-export const addScore = async (req, res) => {
-    const { id } = req.params;
-
-    if (!mongoose.Types.ObjectId.isValid(id)) return res.status(404).send(`No team with id: ${id}`);
-    
-    const player = await Player.findById(id);
-
-    const updatedPlayer = await Player.findByIdAndUpdate(id, { likeCount: player.likeCount + 1 }, { new: true });
-    
-    res.json(updatedPlayer);
-}
-
-export const subScore = async (req, res) => {
-    const { id } = req.params;
-
-    if (!mongoose.Types.ObjectId.isValid(id)) return res.status(404).send(`No team with id: ${id}`);
-    
-    const player = await Player.findById(id);
-
-    const updatedPlayer = await Player.findByIdAndUpdate(id, { likeCount: player.likeCount + 1 }, { new: true });
-    
-    res.json(updatedPlayer);
 }
 
 export default router;
