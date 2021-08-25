d42d6f1fbd443b0624e1d28b18a5867ed424c8f6
ABZZDEV <42191357+Abzzdev@users.noreply.github.com>
2021-08-25 10:13:40 +0100

    Support for a player api url

--- client/src/api/index.js
+++ client/src/api/index.js
@@ -1,7 +1,9 @@
 import axios from 'axios';
 
 const url = 'http://localhost:5000/teams';
+const url2 = 'http://localhost:5000/players';
 
+// Team Management
 export const fetchTeams = () => axios.get(url);
 export const createTeam = (newTeam) => {
     axios.post(url, newTeam)
@@ -11,3 +13,13 @@ export const addScore = (id) => axios.patch(`${url}/${id}/addScore`);
 export const subScore = (id) => axios.patch(`${url}/${id}/subScore`);
 export const updateTeam = (id, updatedTeam) => axios.patch(`${url}/${id}`, updatedTeam);
 export const deleteTeam = (id) => axios.delete(`${url}/${id}`);
+
+
+// Player Management
+export const fetchPlayers = () => axios.get(url2);
+export const createPlayer = (newPlayer) => {
+    axios.post(url2, newPlayer)
+    console.log(newPlayer)
+};
+export const updatePlayer = (id, updatedPlayer) => axios.patch(`${url2}/${id}`, updatedPlayer);
+export const deletePlayer = (id) => axios.delete(`${url2}/${id}`);
