4a067b3492d9b904313408dcc88146c8923ae5ac
ABZZDEV <42191357+Abzzdev@users.noreply.github.com>
2021-08-25 10:16:34 +0100

    Updated player schema

--- server/models/Player.js
+++ server/models/Player.js
@@ -1,21 +1,13 @@
 import mongoose from 'mongoose';
 
-const postSchema = mongoose.Schema({
-    title: String,
-    message: String,
-    creator: String,
-    tags: [String],
+const playerSchema = mongoose.Schema({
+    name: String,
+    kd: String,
+    hs: String,
+    favops: [String],
     selectedFile: String,
-    likeCount: {
-        type: Number,
-        default: 0,
-    },
-    createdAt: {
-        type: Date,
-        default: new Date(),
-    },
 })
 
-var Player = mongoose.model('Player', postSchema);
+var Player = mongoose.model('Player', playerSchema);
 
 export default Player;
