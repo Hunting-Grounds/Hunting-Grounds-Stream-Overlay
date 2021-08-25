b157ae7a96f66ceb8b57c1116702b1b5c418c870
ABZZDEV <42191357+Abzzdev@users.noreply.github.com>
2021-08-25 17:01:08 +0100

    Overlay Menu

--- client/src/components/Overlays/OverlayMenu/OverlayMenu.jsx
+++ client/src/components/Overlays/OverlayMenu/OverlayMenu.jsx
@@ -1,25 +1,31 @@
-import React from 'react'
+import React from 'react';
+import './OverlayMenu.css';
 import 'antd/dist/antd.css';
-import { Divider, Row, Col, Card } from 'antd';
+import { Row, Col, Card } from 'antd';
+import { Link } from 'react-router-dom';
 
 function OverlayMenu() {
     return (
-        <div>
-            <Row justify="space-around" gutter={[24, 24]}>
+        <div className="OverlayBody">
+            <Row justify="space-around" gutter={[24, 24]} align="middle" style={{display: 'flex', height: '100vh'}}>
                 <Col>
-                    <Card>
-                        <a><h1>League Table</h1></a>
-                        <Divider />
-                        <a><h1>League Table</h1></a>
-                        <Divider />
-                        <a><h1>League Table</h1></a>
-                        <Divider />
-                        <a><h1>League Table</h1></a>
-                        <Divider />
-                        <a><h1>League Table</h1></a>
-                        <Divider />
-                        <a><h1>League Table</h1></a>
-                        <Divider />
+                    <Card bordered={false} style={{textAlign: 'center', backgroundColor: 'transparent'}}>
+                        <Link to="/overlays/leaguetable"><p style={{color: 'white', fontSize: 'x-large'}}>League Table</p></Link>
+                        <hr style={{color: 'white'}}/>
+                        <br/>
+                        <Link to="/overlays/leaguetable"><p style={{color: 'white', fontSize: 'x-large'}}>Current Matchup</p></Link>
+                        <hr style={{color: 'white'}}/>
+                        <br/>
+                        <Link to="/overlays/leaguetable"><p style={{color: 'white', fontSize: 'x-large'}}>Playday Upcoming Matches</p></Link>
+                        <hr style={{color: 'white'}}/>
+                        <br/>
+                        <Link to="/overlays/leaguetable"><p style={{color: 'white', fontSize: 'x-large'}}>Webcam & Lower Third</p></Link>
+                        <hr style={{color: 'white'}}/>
+                        <br/>
+                        <Link to="/overlays/leaguetable"><p style={{color: 'white', fontSize: 'x-large'}}>Webcam & Veto</p></Link>
+                        <hr style={{color: 'white'}}/>
+                        <br/>
+                        <Link to="/overlays/leaguetable"><p style={{color: 'white', fontSize: 'x-large'}}>HUD</p></Link>
                     </Card>
                 </Col>
             </Row>
