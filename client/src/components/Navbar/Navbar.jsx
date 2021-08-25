15397d84f7bc2c1ef584a873aa268a8d30ab76f7
ABZZDEV <42191357+Abzzdev@users.noreply.github.com>
2021-08-25 06:37:23 +0100

    Update Navbar.jsx

--- client/src/components/Navbar/Navbar.jsx
+++ client/src/components/Navbar/Navbar.jsx
@@ -12,9 +12,9 @@ function Navbar() {
                 <Header style={{ position: 'fixed', zIndex: 1, width: '100%' }}>
                     <div className="logo" />
                     <Menu theme="dark" mode="horizontal" defaultSelectedKeys={['1']}>
-                        <Menu.Item key="1">nav 1</Menu.Item>
-                        <Menu.Item key="2">nav 2</Menu.Item>
-                        <Menu.Item key="3">nav 3</Menu.Item>
+                        <Menu.Item key="1">Manage Teams</Menu.Item>
+                        <Menu.Item key="2">Manage Players</Menu.Item>
+                        <Menu.Item key="3">Overlays</Menu.Item>
                     </Menu>
                 </Header>
             </Layout>
