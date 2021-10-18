f56603d7fee9f490c1f352e1bb60ab6a7032df93
Abdullah Shah <42191357+ABZZDEV@users.noreply.github.com>
2021-10-18 23:44:38 +0100

    Update fileuploaderController.js

--- server/controllers/fileuploaderController.js
+++ server/controllers/fileuploaderController.js
@@ -5,9 +5,9 @@ import SingleFile from '../models/singlefile.js';
 export const singleFileUpload = async (req, res, next) => {
     try{
         const file = new SingleFile({
-            fileName: req.file.fileName,
-            filePath: req.file.path,
-            fileType: req.file.fileType,
+            fileName: req.fileName,
+            filePath: req.filepath,
+            fileType: req.fileType,
             fileSize: fileSizeFormatter(req.file.size, 2), // 0.00
             fileParent: req.fileParent,
         });
