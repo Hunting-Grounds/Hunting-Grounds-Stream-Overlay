f7795a522e56071c88ea5a940d2df8e86d3d184a
Abdullah Shah <42191357+ABZZDEV@users.noreply.github.com>
2021-10-18 01:00:30 +0100

    Update index.js

--- server/index.js
+++ server/index.js
@@ -17,7 +17,8 @@ app.use('/teams', teamRoutes);
 app.use('/players', playerRoutes);
 app.use('/panels', panelRoutes);
 
-const CONNECTION_URL = 'mongodb+srv://HuntingGroundsDBAdmin:zHYRdt19zXK2x20Z@hunting-grounds-cluster.dpheu.mongodb.net/myFirstDatabase?retryWrites=true&w=majority';
+//const CONNECTION_URL = 'mongodb+srv://HuntingGroundsDBAdmin:zHYRdt19zXK2x20Z@hunting-grounds-cluster.dpheu.mongodb.net/myFirstDatabase?retryWrites=true&w=majority';
+const CONNECTION_URL = 'mongodb://localhost:27017/huntingGrounds';
 const PORT = process.env.PORT|| 5000;
 
 mongoose.connect(CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true })
