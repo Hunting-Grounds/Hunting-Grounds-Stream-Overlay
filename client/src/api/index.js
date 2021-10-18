79593ab5a4cb655ad86d5dc8731cac587067623a
Abdullah Shah <42191357+ABZZDEV@users.noreply.github.com>
2021-10-18 22:00:51 +0100

    Update index.js

--- client/src/api/index.js
+++ client/src/api/index.js
@@ -1,5 +1,4 @@
 import axios from 'axios';
-import axiosConfig from '../helpers/axiosConfig.js'
 
 const url = 'http://195.22.157.230:5000/teams';
 const url2 = 'http://195.22.157.230:5000/players';
@@ -13,6 +12,7 @@ export const createTeam = (newTeam) => {
     axios.post(url, newTeam)
     console.log(newTeam)
 };
+
 export const addScore = (id) => axios.patch(`${url}/${id}/addScore`);
 export const subScore = (id) => axios.patch(`${url}/${id}/subScore`);
 export const updateTeam = (id, updatedTeam) => axios.patch(`${url}/${id}`, updatedTeam);
@@ -25,6 +25,7 @@ export const createPlayer = (newPlayer) => {
     axios.post(url2, newPlayer)
     console.log(newPlayer)
 };
+
 export const updatePlayer = (id, updatedPlayer) => axios.patch(`${url2}/${id}`, updatedPlayer);
 export const deletePlayer = (id) => axios.delete(`${url2}/${id}`);
 
@@ -35,19 +36,20 @@ export const createPanel = (newPanel) => {
     axios.post(url3, newPanel)
     console.log(newPanel)
 };
+
 export const updatePanel = (id, updatedPanel) => axios.patch(`${url3}/${id}`, updatedPanel);
 export const deletePanel = (id) => axios.delete(`${url3}/${id}`);
 
 
 // Image Upload Handling
 export const singleFileUpload = async (data, options, fileParent) => {
-    console.log(data.orignalname)
     try {
         await axios.post(url4 + "upload", data, options, fileParent);
     } catch (error) {
         throw error;
     }
 }
+
 export const getSingleFiles = async () => {
     try {
             const {data} = await axios.get(url4);
