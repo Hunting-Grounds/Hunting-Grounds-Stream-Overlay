38c03b0f89fd0f61090ffd606f76c023c9243776
Abdullah Shah <42191357+ABZZDEV@users.noreply.github.com>
2021-10-19 17:16:42 +0100

    Update MatchupRightTeam.jsx

--- client/src/components/Overlays/CurrentMatchup/MatchupRightTeam.jsx
+++ client/src/components/Overlays/CurrentMatchup/MatchupRightTeam.jsx
@@ -1,14 +1,12 @@
 import React from 'react'
 
+import DBImage from '../Components/DBImage/DBImage'
+
 const MatchupRightTeam = (props) => {
-
-    var LogoURL = 'url(http://195.22.157.230:5000/' + props.file.filePath.toString() + ')'
-
-    console.log(LogoURL)
 
     return (
         <div className="rightTeam" style={{ backgroundColor: props.panelData.currentMatchupColor2 }}>
-            <div className="RightTeamLogo" style={{ backgroundImage: LogoURL }} />
+            <DBImage className="RightTeamLogo" logoUrl={props.file}/>
             <div className="RightTeamName">
                 <h1 className="RightTeamNameText">{props.panelData.currentMatchupName2}</h1>
             </div>
