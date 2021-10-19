4d1ae40902d37ec133941f224ddcc2199fa4d4a4
Abdullah Shah <42191357+ABZZDEV@users.noreply.github.com>
2021-10-19 10:54:30 +0100

    Fixed issues with image db

--- client/src/components/ControlPanel/Panels/Tabs/FileUploadScreen.jsx
+++ client/src/components/ControlPanel/Panels/Tabs/FileUploadScreen.jsx
@@ -26,14 +26,6 @@ const FileUploadScreen = (props) => {
         const formData = new FormData();
         formData.append('file', singleFile);
         formData.append('fileParent', fileParent)
-        for (var value of formData.values()) {
-            console.log(value);
-         }
-
-         for(var pair of formData.entries()) {
-            console.log(pair[0]+ ', '+ pair[1]);
-         }
-
         await singleFileUpload(formData, singleFileOptions);
         props.getsingle();
     }
