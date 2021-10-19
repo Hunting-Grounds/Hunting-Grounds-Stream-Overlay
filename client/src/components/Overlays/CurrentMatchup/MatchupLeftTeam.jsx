dfa89f960db9b865356f9a16755a206e37c97476
Abdullah Shah <42191357+ABZZDEV@users.noreply.github.com>
2021-10-19 15:50:18 +0100

    Fix

--- client/src/components/Overlays/CurrentMatchup/MatchupLeftTeam.jsx
+++ client/src/components/Overlays/CurrentMatchup/MatchupLeftTeam.jsx
@@ -2,12 +2,12 @@ import React from 'react'
 
 const MatchupLeftTeam = (props) => {
 
-    var LogoURL = props.file.filePath.toString()
+    var LogoURL = 'url(http://195.22.157.230:5000/' + props.file.filePath.toString() + ')'
 
     return (
         <div className="leftTeam" style={{ backgroundColor: props.panelData.currentMatchupColor1 }}>
             {console.log(props.file.filePath)}
-            <div className="LeftTeamLogo" style={{ backgroundImage: `url(http://195.22.157.230:5000/${LogoURL})` }} />
+            <div className="LeftTeamLogo" style={{ backgroundImage: LogoURL }} />
             <div className="LeftTeamName">
                 <h1 className="LeftTeamNameText">{props.panelData.currentMatchupName1}</h1>
             </div>
--- client/src/components/Overlays/CurrentMatchup/MatchupRightTeam.jsx
+++ client/src/components/Overlays/CurrentMatchup/MatchupRightTeam.jsx
@@ -2,13 +2,13 @@ import React from 'react'
 
 const MatchupRightTeam = (props) => {
 
-    var LogoURL = props.file.filePath.toString()
+    var LogoURL = 'url(http://195.22.157.230:5000/' + props.file.filePath.toString() + ')'
 
     console.log(LogoURL)
 
     return (
         <div className="rightTeam" style={{ backgroundColor: props.panelData.currentMatchupColor2 }}>
-            <div className="RightTeamLogo" style={{ backgroundImage: `url(http://195.22.157.230:5000/${LogoURL})` }} />
+            <div className="RightTeamLogo" style={{ backgroundImage: LogoURL }} />
             <div className="RightTeamName">
                 <h1 className="RightTeamNameText">{props.panelData.currentMatchupName2}</h1>
             </div>
