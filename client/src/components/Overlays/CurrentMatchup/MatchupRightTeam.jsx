683798c7ace74cf930c3bb8d006d3f1dbb425b98
Abdullah Shah <42191357+ABZZDEV@users.noreply.github.com>
2021-10-19 15:45:41 +0100

    Fix

--- client/src/components/Overlays/CurrentMatchup/MatchupRightTeam.jsx
+++ client/src/components/Overlays/CurrentMatchup/MatchupRightTeam.jsx
@@ -2,12 +2,13 @@ import React from 'react'
 
 const MatchupRightTeam = (props) => {
 
-    console.log(props.file.filePath)
-    const currentMacthupLogoURL = `http://195.22.157.230:5000/${props.file.filePath}`
+    var LogoURL = props.file.filePath.toString()
+
+    console.log(LogoURL)
 
     return (
         <div className="rightTeam" style={{ backgroundColor: props.panelData.currentMatchupColor2 }}>
-            <div className="RightTeamLogo" style={{ backgroundImage: `url(http://195.22.157.230:5000/${props.file.filePath})` }} />
+            <div className="RightTeamLogo" style={{ backgroundImage: `url(http://195.22.157.230:5000/${LogoURL})` }} />
             <div className="RightTeamName">
                 <h1 className="RightTeamNameText">{props.panelData.currentMatchupName2}</h1>
             </div>
