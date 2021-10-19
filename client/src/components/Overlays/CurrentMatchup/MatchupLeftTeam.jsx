1f507a235808694dbbe1da0ab4c77408fe4e72d4
Abdullah Shah <42191357+ABZZDEV@users.noreply.github.com>
2021-10-19 15:32:15 +0100

    Update MatchupLeftTeam.jsx

--- client/src/components/Overlays/CurrentMatchup/MatchupLeftTeam.jsx
+++ client/src/components/Overlays/CurrentMatchup/MatchupLeftTeam.jsx
@@ -2,10 +2,9 @@ import React from 'react'
 
 const MatchupLeftTeam = (props) => {
 
-    console.log(props.file.filePath)
-
     return (
         <div className="leftTeam" style={{ backgroundColor: props.panelData.currentMatchupColor1 }}>
+            {console.log(props.file.filePath)}
             <div className="LeftTeamLogo" style={{ backgroundImage: `url(http://195.22.157.230:5000/${props.file.filePath})` }} />
             <div className="LeftTeamName">
                 <h1 className="LeftTeamNameText">{props.panelData.currentMatchupName1}</h1>
