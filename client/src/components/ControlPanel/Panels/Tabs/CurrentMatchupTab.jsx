537d7e31860d1bed28cf3886c8e7faea37a180cb
Abdullah Shah <42191357+ABZZDEV@users.noreply.github.com>
2021-10-19 11:11:05 +0100

    Update CurrentMatchupTab.jsx

--- client/src/components/ControlPanel/Panels/Tabs/CurrentMatchupTab.jsx
+++ client/src/components/ControlPanel/Panels/Tabs/CurrentMatchupTab.jsx
@@ -76,7 +76,7 @@ const CurrentMatchupTab = ({ currentId, setCurrentId }) => {
             <FileUploadScreen getsingle={() => getSingleFileslist()} fileParent="currentMatchupLogo1" />
             {singleFiles.filter(file => file.fileParent === "currentMatchupLogo1").map((file, index) =>
               <Card
-              style={{ width: '30vw' }}
+              style={{ width: '20vw', position: 'absolute', left: '10' }}
               cover={
                 <img
                   alt={file.fileParent}
