ed6e29e4cb0ec3f18076efbb91f578e22b1c87e0
Abdullah Shah <42191357+ABZZDEV@users.noreply.github.com>
2021-10-19 17:29:48 +0100

    Update DBImage.jsx

--- client/src/components/Overlays/Components/DBImage/DBImage.jsx
+++ client/src/components/Overlays/Components/DBImage/DBImage.jsx
@@ -2,8 +2,6 @@ import React from 'react'
 
 const DBImage = (props) => {
 
-    console.log(props.file)
-
     var LogoURL = 'url(http://195.22.157.230:5000/' + props.file.filePath.toString() + ')'
 
     return (
