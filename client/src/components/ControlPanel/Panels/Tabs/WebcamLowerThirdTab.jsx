459e2f75d633640017562c9bc2f9639fb5740763
Abdullah Shah <42191357+ABZZDEV@users.noreply.github.com>
2021-10-19 23:54:39 +0100

    Update WebcamLowerThirdTab.jsx

--- client/src/components/ControlPanel/Panels/Tabs/WebcamLowerThirdTab.jsx
+++ client/src/components/ControlPanel/Panels/Tabs/WebcamLowerThirdTab.jsx
@@ -5,6 +5,8 @@ import { useDispatch, useSelector } from 'react-redux';
 import 'antd/dist/antd.css';
 import { Select } from 'antd';
 
+import { Row, Col } from 'antd';
+
 import useStyles from './styles';
 import { updatePanel } from '../../../../actions/panels';
 
@@ -94,46 +96,34 @@ const WebcamLowerThirdTab = ({ currentId, setCurrentId, currentTeamId, setCurren
     <Paper className={classes.paper}>
       <form autoComplete="off" noValidate className={`${classes.root} ${classes.form}`} onSubmit={handleSubmit}>
         <Typography variant="h6">Editing Webcam & Lower Third</Typography>
-        <TextField name="webcamCasterName1" variant="outlined" label="Webcam Caster Name 1" fullWidth value={panelData.webcamCasterName1} onChange={(e) => setPanelData({ ...panelData, webcamCasterName1: e.target.value })} />
-        <TextField name="webcamCasterName2" variant="outlined" label="Webcam Caster Name 2" fullWidth value={panelData.webcamCasterName2} onChange={(e) => setPanelData({ ...panelData, webcamCasterName2: e.target.value })} />
-        <TextField name="lowerThirdTitle" variant="outlined" label="Lower Third Title" fullWidth value={panelData.lowerThirdTitle} onChange={(e) => setPanelData({ ...panelData, lowerThirdTitle: e.target.value })} />
-        <TextField name="lowerThirdSubtitle" variant="outlined" label="Lower Third Subtitle" fullWidth value={panelData.lowerThirdSubtitle} onChange={(e) => setPanelData({ ...panelData, lowerThirdSubtitle: e.target.value })} />
-        <TextField name="lowerThirdTeamName1" variant="outlined" label="Lower Third Team Name 1" fullWidth value={panelData.lowerThirdTeamName1} onChange={(e) => setPanelData({ ...panelData, lowerThirdTeamName1: e.target.value })} />
-
-        <Select
-          showSearch
-          style={{ width: '100%' }}
-          size="large"
-          placeholder="Select a team"
-          optionFilterProp="children"
-          onChange={onChange}
-          onFocus={onFocus}
-          onBlur={onBlur}
-          onSearch={onSearch}
-          filterOption={(input, option) =>
-            option.children.toLowerCase().indexOf(input.toLowerCase()) >= 0
-          }
-        >
-          {teams.map((team) => (
-            console.log('LOG:' + team._id),
-            <Option value={team.fullName}> {team.fullName}</Option>
-          ))}
-        </Select>
-
-        <FileUploadScreen getsingle={() => getSingleFileslist()} fileParent="lowerThirdTeamLogo1" fieldName={"Lower Third Team Logo 1"} />
-        {singleFiles.filter(file => file.fileParent === "lowerThirdTeamLogo1").map((file, index) =>
-          <FileCard getsingle={() => getSingleFileslist()} file={file} />
-        )}
-
-        <TextField name="lowerThirdTeamColor1" variant="outlined" label="Lower Third Team Color 1" fullWidth value={panelData.lowerThirdTeamColor1} onChange={(e) => setPanelData({ ...panelData, lowerThirdTeamColor1: e.target.value })} />
-        <TextField name="lowerThirdTeamName2" variant="outlined" label="Lower Third Team Name 2" fullWidth value={panelData.lowerThirdTeamName2} onChange={(e) => setPanelData({ ...panelData, lowerThirdTeamName2: e.target.value })} />
-
-        <FileUploadScreen getsingle={() => getSingleFileslist()} fileParent="lowerThirdTeamLogo2" fieldName={"Lower Third Team Logo 2"} />
-        {singleFiles.filter(file => file.fileParent === "lowerThirdTeamLogo2").map((file, index) =>
-          <FileCard getsingle={() => getSingleFileslist()} file={file} />
-        )}
-
-        <TextField name="lowerThirdTeamColor2" variant="outlined" label="Lower Third Team Color 2" fullWidth value={panelData.lowerThirdTeamColor2} onChange={(e) => setPanelData({ ...panelData, lowerThirdTeamColor2: e.target.value })} />
+        <Row justify="space-around" align="middle" style={{ width: '100%' }}>
+          <Col span={12}>
+            <TextField name="webcamCasterName1" variant="outlined" label="Webcam Caster Name 1" fullWidth value={panelData.webcamCasterName1} onChange={(e) => setPanelData({ ...panelData, webcamCasterName1: e.target.value })} />
+            <TextField name="webcamCasterName2" variant="outlined" label="Webcam Caster Name 2" fullWidth value={panelData.webcamCasterName2} onChange={(e) => setPanelData({ ...panelData, webcamCasterName2: e.target.value })} />
+            <TextField name="lowerThirdTeamName1" variant="outlined" label="Lower Third Team Name 1" fullWidth value={panelData.lowerThirdTeamName1} onChange={(e) => setPanelData({ ...panelData, lowerThirdTeamName1: e.target.value })} />
+            <TextField name="lowerThirdTeamColor1" variant="outlined" label="Lower Third Team Color 1" fullWidth value={panelData.lowerThirdTeamColor1} onChange={(e) => setPanelData({ ...panelData, lowerThirdTeamColor1: e.target.value })} />
+            <Row justify="space-around" align="middle" style={{ width: '100%' }}>
+                {!singleFiles.filter(file => file.fileParent === "lowerThirdTeamLogo1").length ? <FileUploadScreen getsingle={() => getSingleFileslist()} fileParent="lowerThirdTeamLogo1" fieldName={"Lower Third Team Logo 1"} /> : (
+                  singleFiles.filter(file => file.fileParent === "lowerThirdTeamLogo1").map((file, index) =>
+                    <FileCard getsingle={() => getSingleFileslist()} file={file} fieldName={"Lower Third Team Logo 1"} />
+                  )
+                )}
+            </Row>
+          </Col>
+          <Col span={12}>
+            <TextField name="lowerThirdTitle" variant="outlined" label="Lower Third Title" fullWidth value={panelData.lowerThirdTitle} onChange={(e) => setPanelData({ ...panelData, lowerThirdTitle: e.target.value })} />
+            <TextField name="lowerThirdSubtitle" variant="outlined" label="Lower Third Subtitle" fullWidth value={panelData.lowerThirdSubtitle} onChange={(e) => setPanelData({ ...panelData, lowerThirdSubtitle: e.target.value })} />
+            <TextField name="lowerThirdTeamName2" variant="outlined" label="Lower Third Team Name 2" fullWidth value={panelData.lowerThirdTeamName2} onChange={(e) => setPanelData({ ...panelData, lowerThirdTeamName2: e.target.value })} />
+            <TextField name="lowerThirdTeamColor2" variant="outlined" label="Lower Third Team Color 2" fullWidth value={panelData.lowerThirdTeamColor2} onChange={(e) => setPanelData({ ...panelData, lowerThirdTeamColor2: e.target.value })} />
+            <Row justify="space-around" align="middle" style={{ width: '100%' }}>
+                {!singleFiles.filter(file => file.fileParent === "lowerThirdTeamLogo2").length ? <FileUploadScreen getsingle={() => getSingleFileslist()} fileParent="lowerThirdTeamLogo2" fieldName={"Lower Third Team Logo 2"} /> : (
+                  singleFiles.filter(file => file.fileParent === "lowerThirdTeamLogo2").map((file, index) =>
+                    <FileCard getsingle={() => getSingleFileslist()} file={file} fieldName={"Lower Third Team Logo 2"} />
+                  )
+                )}
+            </Row>
+          </Col>
+        </Row>
         <Button className={classes.buttonSubmit} variant="contained" color="primary" size="large" type="submit" fullWidth>Submit</Button>
         <Button variant="contained" color="secondary" size="small" onClick={clear} fullWidth>Clear</Button>
       </form>
