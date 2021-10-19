7369eb6cddba4235caa3b366702fc625df7d9959
Abdullah Shah <42191357+ABZZDEV@users.noreply.github.com>
2021-10-19 22:57:52 +0100

    Update FileCard.jsx

--- client/src/components/ControlPanel/Panels/Tabs/Components/FileCard.jsx
+++ client/src/components/ControlPanel/Panels/Tabs/Components/FileCard.jsx
@@ -17,16 +17,17 @@ const FileCard = (props) => {
     }
 
     return (
-        <Card
-            style={{ width: '20vw', height: '20vh', overflow: 'hidden'}}
-            cover={
-                <img style={{display: 'block', marginLeft: 'auto', marginRight: 'auto', height: '80%', width: ''}}
-                    src={`http://195.22.157.230:5000/${props.file.filePath}`}
-                />
-            }
-            actions={[<a onClick={() => deleteSingleFile(props.file._id) } style={{zIndex: '3'}}><DeleteOutlined style={{zIndex: '3'}} key="delete" /></a>]}
-        >
-        </Card>
+        <div>
+            <Card
+                style={{ width: '20vw', height: '20vh', overflow: 'hidden'}}
+                cover={
+                    <img style={{display: 'block', marginLeft: 'auto', marginRight: 'auto', height: '80%', width: ''}}
+                        src={`http://195.22.157.230:5000/${props.file.filePath}`}
+                    />
+                }
+            />
+            <Card actions={[<a onClick={() => deleteSingleFile(props.file._id) } style={{zIndex: '3'}}><DeleteOutlined style={{zIndex: '3'}} key="delete" /></a>]}/>
+        </div>
     )
 }
 
