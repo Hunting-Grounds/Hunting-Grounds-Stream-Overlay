ea69a69b59004cfaf82cb40696db016771911a14
Abdullah Shah <42191357+ABZZDEV@users.noreply.github.com>
2021-10-19 17:20:19 +0100

    Update MatchupRightTeam.jsx

--- client/src/components/Overlays/CurrentMatchup/MatchupRightTeam.jsx
+++ client/src/components/Overlays/CurrentMatchup/MatchupRightTeam.jsx
@@ -6,6 +6,7 @@ const MatchupRightTeam = (props) => {
 
     return (
         <div className="rightTeam" style={{ backgroundColor: props.panelData.currentMatchupColor2 }}>
+            {console.log(props.file)}
             <DBImage className="RightTeamLogo" logoUrl={props.file}/>
             <div className="RightTeamName">
                 <h1 className="RightTeamNameText">{props.panelData.currentMatchupName2}</h1>
