2544555ec2e53024f6539d0a13751e4ce523eee6
Abdullah Shah <42191357+ABZZDEV@users.noreply.github.com>
2021-10-19 13:20:02 +0100

    Update UploadButton.jsx

--- client/src/components/ControlPanel/Panels/Tabs/Components/UploadButton.jsx
+++ client/src/components/ControlPanel/Panels/Tabs/Components/UploadButton.jsx
@@ -1,7 +1,5 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
 import 'antd/dist/antd.css';
-import './index.css';
 import { Upload, Modal } from 'antd';
 import { PlusOutlined } from '@ant-design/icons';
 
