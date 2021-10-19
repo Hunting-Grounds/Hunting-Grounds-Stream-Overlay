7192ce9ae0d87147645c6970fbd7aaf191162c8d
Abdullah Shah <42191357+ABZZDEV@users.noreply.github.com>
2021-10-19 13:33:25 +0100

    .

--- client/src/components/ControlPanel/Panels/Tabs/Components/FileUploadScreen.jsx
+++ client/src/components/ControlPanel/Panels/Tabs/Components/FileUploadScreen.jsx
@@ -7,36 +7,11 @@ import UploadButton from './UploadButton'
 
 const FileUploadScreen = (props) => {
 
-    const [singleFile, setSingleFile] = useState({});
-    const [singleProgress, setSingleProgress] = useState(0);
-
-    const SingleFileChange = (e) => {
-        setSingleFile(e.target.files[0]);
-        setSingleProgress(0);
-    }
-
-    const singleFileOptions = {
-        onUploadProgress: (progressEvent) => {
-            const { loaded, total } = progressEvent;
-            const percentage = Math.floor(((loaded / 1000) * 100) / (total / 1000));
-            setSingleProgress(percentage);
-        }
-    }
-
-    const uploadSingleFile = async (fileParent) => {
-        console.log(fileParent)
-        const formData = new FormData();
-        formData.append('file', singleFile);
-        formData.append('fileParent', fileParent)
-        await singleFileUpload(formData, singleFileOptions);
-        props.getsingle();
-    }
-
     return (
         <div>
             <Row>
                 <Col>
-                    <UploadButton/>
+                    <UploadButton {...props}/>
                 </Col>
             </Row>
             <Row>
--- client/src/components/ControlPanel/Panels/Tabs/Components/UploadButton.jsx
+++ client/src/components/ControlPanel/Panels/Tabs/Components/UploadButton.jsx
@@ -4,75 +4,102 @@ import { Upload, Modal } from 'antd';
 import { PlusOutlined } from '@ant-design/icons';
 
 function getBase64(file) {
-  return new Promise((resolve, reject) => {
-    const reader = new FileReader();
-    reader.readAsDataURL(file);
-    reader.onload = () => resolve(reader.result);
-    reader.onerror = error => reject(error);
-  });
+    return new Promise((resolve, reject) => {
+        const reader = new FileReader();
+        reader.readAsDataURL(file);
+        reader.onload = () => resolve(reader.result);
+        reader.onerror = error => reject(error);
+    });
 }
 
+const[singleFile, setSingleFile] = useState({ });
+const[singleProgress, setSingleProgress] = useState(0);
+
 class UploadButton extends React.Component {
-  state = {
-    previewVisible: false,
-    previewImage: '',
-    previewTitle: 'Test',
-    fileList: [
-      {
-        uid: '-1',
-        name: 'image.png',
-        status: 'done',
-        url: 'https://zos.alipayobjects.com/rmsportal/jkjgkEfvpUPVyRjUImniVslZfWPnJuuZ.png',
-      },
-    ],
-  };
 
-  handleCancel = () => this.setState({ previewVisible: false });
-
-  handlePreview = async file => {
-    if (!file.url && !file.preview) {
-      file.preview = await getBase64(file.originFileObj);
+    SingleFileChange = (e) => {
+        setSingleFile(e.target.files[0]);
+        setSingleProgress(0);
     }
 
-    this.setState({
-      previewImage: file.url || file.preview,
-      previewVisible: true,
-      previewTitle: file.name || file.url.substring(file.url.lastIndexOf('/') + 1),
-    });
-  };
+    singleFileOptions = {
+        onUploadProgress: (progressEvent) => {
+            const { loaded, total } = progressEvent;
+            const percentage = Math.floor(((loaded / 1000) * 100) / (total / 1000));
+            setSingleProgress(percentage);
+        }
+    }
 
-  handleChange = ({ fileList }) => this.setState({ fileList });
+    uploadSingleFile = async (fileParent) => {
+        console.log(fileParent)
+        const formData = new FormData();
+        formData.append('file', singleFile);
+        formData.append('fileParent', fileParent)
+        await singleFileUpload(formData, singleFileOptions);
+        this.props.getsingle();
+    }
 
-  render() {
-    const { previewVisible, previewImage, fileList, previewTitle } = this.state;
-    const uploadButton = (
-      <div>
-        <PlusOutlined />
-        <div style={{ marginTop: 8 }}>Upload</div>
-      </div>
-    );
-    return (
-      <>
-        <Upload
-          action="https://www.mocky.io/v2/5cc8019d300000980a055e76"
-          listType="picture-card"
-          fileList={fileList}
-          onPreview={this.handlePreview}
-          onChange={this.handleChange}
-        >
-          {fileList.length >= 1 ? null : uploadButton}
-        </Upload>
-        <Modal
-          visible={previewVisible}
-          title={previewTitle}
-          footer={null}
-          onCancel={this.handleCancel}
-        >
-          <img alt="example" style={{ width: '100%' }} src={previewImage} />
-        </Modal>
-      </>
-    );
-  }
+
+    state = {
+        previewVisible: false,
+        previewImage: '',
+        previewTitle: 'Test',
+        fileList: [
+            {
+                uid: '1',
+                name: 'image.png',
+                status: 'done',
+                url: 'https://zos.alipayobjects.com/rmsportal/jkjgkEfvpUPVyRjUImniVslZfWPnJuuZ.png',
+            },
+        ],
+    };
+
+    handleCancel = () => this.setState({ previewVisible: false });
+
+    handlePreview = async file => {
+        if (!file.url && !file.preview) {
+            file.preview = await getBase64(file.originFileObj);
+        }
+
+        this.setState({
+            previewImage: file.url || file.preview,
+            previewVisible: true,
+            previewTitle: file.name || file.url.substring(file.url.lastIndexOf('/') + 1),
+        });
+    };
+
+    handleChange = ({ fileList }) => this.setState({ fileList });
+
+    render() {
+        const { previewVisible, previewImage, fileList, previewTitle } = this.state;
+        const uploadButton = (
+            <div>
+                <PlusOutlined />
+                <div style={{ marginTop: 8 }}>Upload</div>
+            </div>
+        );
+        return (
+            <>
+                <Upload
+                    action={() => uploadSingleFile(this.props.fileParent)}
+                    listType="picture-card"
+                    fileList={fileList}
+                    onPreview={this.handlePreview}
+                    onChange={this.handleChange}
+                >
+                    {fileList.length >= 1 ? null : uploadButton}
+                </Upload>
+                <Modal
+                    visible={previewVisible}
+                    title={previewTitle}
+                    footer={null}
+                    onCancel={this.handleCancel}
+                >
+                    <img alt="example" style={{ width: '100%' }} src={previewImage} />
+                </Modal>
+            </>
+        );
+    }
 }
 
 export default UploadButton;
