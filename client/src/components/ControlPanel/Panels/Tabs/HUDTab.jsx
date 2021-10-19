3d25a97de17d749e0f4e0c869adb44c0394f2d8d
Abdullah Shah <42191357+ABZZDEV@users.noreply.github.com>
2021-10-19 19:08:39 +0100

    Lower Third

--- client/src/components/ControlPanel/Panels/Tabs/HUDTab.jsx
+++ client/src/components/ControlPanel/Panels/Tabs/HUDTab.jsx
@@ -1,7 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import { TextField, Button, Typography, Paper } from '@material-ui/core';
 import { useDispatch, useSelector } from 'react-redux';
-import FileBase from 'react-file-base64';
 
 import useStyles from './styles';
 import { updatePanel } from '../../../../actions/panels';
--- client/src/components/ControlPanel/Panels/Tabs/WebcamLowerThirdTab.jsx
+++ client/src/components/ControlPanel/Panels/Tabs/WebcamLowerThirdTab.jsx
@@ -1,7 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import { TextField, Button, Typography, Paper } from '@material-ui/core';
 import { useDispatch, useSelector } from 'react-redux';
-import FileBase from 'react-file-base64';
 
 import 'antd/dist/antd.css';
 import { Select } from 'antd';
@@ -11,7 +10,30 @@ import { updatePanel } from '../../../../actions/panels';
 
 const { Option } = Select;
 
+import FileUploadScreen from './Components/FileUploadScreen';
+import FileCard from './Components/FileCard';
+import { getSingleFiles } from '../../../../api/index.js';
+
 const WebcamLowerThirdTab = ({ currentId, setCurrentId, currentTeamId, setCurrentTeamId }) => {
+
+  /////// IMAGE STUFF ////////
+
+  const [singleFiles, setSingleFiles] = useState([]);
+
+  const getSingleFileslist = async () => {
+    try {
+      const fileslist = await getSingleFiles();
+      setSingleFiles(fileslist);
+    } catch (error) {
+      console.log(error);
+    }
+  }
+
+  useEffect(() => {
+    getSingleFileslist();
+  }, []);
+
+  ///////////////////////////
 
   const teams = useSelector((state) => state.teams);
 
@@ -94,18 +116,27 @@ const WebcamLowerThirdTab = ({ currentId, setCurrentId, currentTeamId, setCurren
         >
           {teams.map((team) => (
             console.log('LOG:' + team._id),
-            <Option value = {team.fullName}> {team.fullName}</Option>
+            <Option value={team.fullName}> {team.fullName}</Option>
           ))}
-      </Select>
+        </Select>
 
-      <div className={classes.fileInput}>Lower Third Team Logo 1 <FileBase type="file" multiple={false} onDone={({ base64 }) => setPanelData({ ...panelData, lowerThirdTeamLogo1: base64 })} /></div>
-      <TextField name="lowerThirdTeamColor1" variant="outlined" label="Lower Third Team Color 1" fullWidth value={panelData.lowerThirdTeamColor1} onChange={(e) => setPanelData({ ...panelData, lowerThirdTeamColor1: e.target.value })} />
-      <TextField name="lowerThirdTeamName2" variant="outlined" label="Lower Third Team Name 2" fullWidth value={panelData.lowerThirdTeamName2} onChange={(e) => setPanelData({ ...panelData, lowerThirdTeamName2: e.target.value })} />
-      <div className={classes.fileInput}>Lower Third Team Logo 2 <FileBase type="file" multiple={false} onDone={({ base64 }) => setPanelData({ ...panelData, lowerThirdTeamLogo2: base64 })} /></div>
-      <TextField name="lowerThirdTeamColor2" variant="outlined" label="Lower Third Team Color 2" fullWidth value={panelData.lowerThirdTeamColor2} onChange={(e) => setPanelData({ ...panelData, lowerThirdTeamColor2: e.target.value })} />
-      <Button className={classes.buttonSubmit} variant="contained" color="primary" size="large" type="submit" fullWidth>Submit</Button>
-      <Button variant="contained" color="secondary" size="small" onClick={clear} fullWidth>Clear</Button>
-    </form>
+        <FileUploadScreen getsingle={() => getSingleFileslist()} fileParent="lowerThirdTeamLogo1" fieldName={"Lower Third Team Logo 1"} />
+        {singleFiles.filter(file => file.fileParent === "lowerThirdTeamLogo1").map((file, index) =>
+          <FileCard getsingle={() => getSingleFileslist()} file={file} />
+        )}
+
+        <TextField name="lowerThirdTeamColor1" variant="outlined" label="Lower Third Team Color 1" fullWidth value={panelData.lowerThirdTeamColor1} onChange={(e) => setPanelData({ ...panelData, lowerThirdTeamColor1: e.target.value })} />
+        <TextField name="lowerThirdTeamName2" variant="outlined" label="Lower Third Team Name 2" fullWidth value={panelData.lowerThirdTeamName2} onChange={(e) => setPanelData({ ...panelData, lowerThirdTeamName2: e.target.value })} />
+
+        <FileUploadScreen getsingle={() => getSingleFileslist()} fileParent="lowerThirdTeamLogo2" fieldName={"Lower Third Team Logo 2"} />
+        {singleFiles.filter(file => file.fileParent === "lowerThirdTeamLogo2").map((file, index) =>
+          <FileCard getsingle={() => getSingleFileslist()} file={file} />
+        )}
+
+        <TextField name="lowerThirdTeamColor2" variant="outlined" label="Lower Third Team Color 2" fullWidth value={panelData.lowerThirdTeamColor2} onChange={(e) => setPanelData({ ...panelData, lowerThirdTeamColor2: e.target.value })} />
+        <Button className={classes.buttonSubmit} variant="contained" color="primary" size="large" type="submit" fullWidth>Submit</Button>
+        <Button variant="contained" color="secondary" size="small" onClick={clear} fullWidth>Clear</Button>
+      </form>
     </Paper >
   );
 };
