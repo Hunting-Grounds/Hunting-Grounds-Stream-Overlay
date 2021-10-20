549f723e2566f577ee5963d043de7bdf16b072e0
Abdullah Shah <42191357+ABZZDEV@users.noreply.github.com>
2021-10-20 19:06:40 +0100

    Vertical Align

--- client/src/components/Overlays/LeagueTable/LeagueTableBody.jsx
+++ client/src/components/Overlays/LeagueTable/LeagueTableBody.jsx
@@ -40,91 +40,123 @@ const LeagueTableBody = ({ currentId, setCurrentId }) => {
                 </Col>
             </Row>
             <Row className="LeagueTableRow Row1">
-                <Col span={14} className="LeagueTableCol1" style={{backgroundColor: 'orange'}}>
+                <Col span={14} className="LeagueTableCol1" style={{ backgroundColor: 'orange' }}>
                     {panelData.leagueTableNameRow1}
                 </Col>
                 <Col span={6} className="LeagueTableCol2Black">
-                    {panelData.leagueTableWLRow1}
+                    <span className="VerticalAlign">
+                        {panelData.leagueTableWLRow1}
+                    </span>
                 </Col>
                 <Col span={4} className="LeagueTableCol3">
-                    {panelData.leagueTablePTSRow1}
+                    <span className="VerticalAlign">
+                        {panelData.leagueTablePTSRow1}
+                    </span>
                 </Col>
             </Row>
-            <Row className="LeagueTableRow Row2" style={{backgroundColor: 'lightskyblue'}}>
+            <Row className="LeagueTableRow Row2" style={{ backgroundColor: 'lightskyblue' }}>
                 <Col span={14} className="LeagueTableCol1">
                     {panelData.leagueTableNameRow2}
                 </Col>
                 <Col span={6} className="LeagueTableCol2Grey">
-                    {panelData.leagueTableWLRow2}
+                    <span className='VerticalAlign'>
+                        {panelData.leagueTableWLRow2}
+                    </span>
                 </Col>
                 <Col span={4} className="LeagueTableCol3">
-                    {panelData.leagueTablePTSRow2}
+                    <span className="VerticalAlign">
+                        {panelData.leagueTablePTSRow2}
+                    </span>
                 </Col>
             </Row>
-            <Row className="LeagueTableRow Row3" style={{backgroundColor: 'lightskyblue'}}>
+            <Row className="LeagueTableRow Row3" style={{ backgroundColor: 'lightskyblue' }}>
                 <Col span={14} className="LeagueTableCol1">
                     {panelData.leagueTableNameRow3}
                 </Col>
                 <Col span={6} className="LeagueTableCol2Black">
-                    {panelData.leagueTableWLRow3}
+                    <span className="VerticalAlign">
+                        {panelData.leagueTableWLRow3}
+                    </span>
                 </Col>
                 <Col span={4} className="LeagueTableCol3">
-                    {panelData.leagueTablePTSRow3}
+                    <span className="VerticalAlign">
+                        {panelData.leagueTablePTSRow3}
+                    </span>
                 </Col>
             </Row>
-            <Row className="LeagueTableRow Row4" style={{backgroundColor: 'lightskyblue'}}>
+            <Row className="LeagueTableRow Row4" style={{ backgroundColor: 'lightskyblue' }}>
                 <Col span={14} className="LeagueTableCol1">
                     {panelData.leagueTableNameRow4}
                 </Col>
                 <Col span={6} className="LeagueTableCol2Grey">
-                    {panelData.leagueTableWLRow4}
+                    <span className="VerticalAlign">
+                        {panelData.leagueTableWLRow4}
+                    </span>
                 </Col>
                 <Col span={4} className="LeagueTableCol3">
-                    {panelData.leagueTablePTSRow4}
+                    <span className="VerticalAlign">
+                        {panelData.leagueTablePTSRow4}
+                    </span>
                 </Col>
             </Row>
-            <Row className="LeagueTableRow Row5" style={{backgroundColor: 'lightskyblue'}}>
+            <Row className="LeagueTableRow Row5" style={{ backgroundColor: 'lightskyblue' }}>
                 <Col span={14} className="LeagueTableCol1">
                     {panelData.leagueTableNameRow5}
                 </Col>
                 <Col span={6} className="LeagueTableCol2Black">
-                    {panelData.leagueTableWLRow5}
+                    <span className="VerticalAlign">
+                        {panelData.leagueTableWLRow5}
+                    </span>
                 </Col>
                 <Col span={4} className="LeagueTableCol3">
-                    {panelData.leagueTablePTSRow5}
+                    <span className="VerticalAlign">
+                        {panelData.leagueTablePTSRow5}
+                    </span>
                 </Col>
             </Row>
-            <Row className="LeagueTableRow Row6" style={{backgroundColor: 'lightskyblue'}}>
+            <Row className="LeagueTableRow Row6" style={{ backgroundColor: 'lightskyblue' }}>
                 <Col span={14} className="LeagueTableCol1">
                     {panelData.leagueTableNameRow6}
                 </Col>
                 <Col span={6} className="LeagueTableCol2Grey">
-                    {panelData.leagueTableWLRow6}
+                    <span className="VerticalAlign">
+                        {panelData.leagueTableWLRow6}
+                    </span>
                 </Col>
                 <Col span={4} className="LeagueTableCol3">
-                    {panelData.leagueTablePTSRow6}
+                    <span className="VerticalAlign">
+                        {panelData.leagueTablePTSRow6}
+                    </span>
                 </Col>
             </Row>
-            <Row className="LeagueTableRow Row7" style={{backgroundColor: 'lightskyblue'}}>
+            <Row className="LeagueTableRow Row7" style={{ backgroundColor: 'lightskyblue' }}>
                 <Col span={14} className="LeagueTableCol1">
                     {panelData.leagueTableNameRow7}
                 </Col>
                 <Col span={6} className="LeagueTableCol2Black">
-                    {panelData.leagueTableWLRow7}
+                    <span className="VerticalAlign">
+                        {panelData.leagueTableWLRow7}
+                    </span>
                 </Col>
                 <Col span={4} className="LeagueTableCol3">
-                    {panelData.leagueTablePTSRow7}
+                    <span className="VerticalAlign">
+                        {panelData.leagueTablePTSRow7}
+                    </span>
                 </Col>
             </Row>
-            <Row className="LeagueTableRow Row8" style={{backgroundColor: 'lightskyblue'}}>
+            <Row className="LeagueTableRow Row8" style={{ backgroundColor: 'lightskyblue' }}>
                 <Col span={14} className="LeagueTableCol1">
                     {panelData.leagueTableNameRow8}
                 </Col>
                 <Col span={6} className="LeagueTableCol2Grey">
-                    {panelData.leagueTableWLRow8}
+                    <span className="VerticalAlign">
+                        {panelData.leagueTableWLRow8}
+                    </span>
                 </Col>
                 <Col span={4} className="LeagueTableCol3">
-                    {panelData.leagueTablePTSRow8}
+                    <span className="VerticalAlign">
+                        {panelData.leagueTablePTSRow8}
+                    </span>
                 </Col>
             </Row>
         </div>
